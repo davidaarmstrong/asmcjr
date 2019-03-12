c80ccfc485ec4c4994fd59b0a51fc7ce6d14c4aa
davidaarmstrong <dave@quantoid.net>
2019-03-12 12:35:50 -0400

    fix

--- R/asmcjr.r
+++ R/asmcjr.r
@@ -3,7 +3,7 @@ gray.palette <- function(n, lower=.3, upper=.7){
     s <- seq(lower, upper, length=n)
     rgb(matrix(rep(s, each=3), ncol=3, byrow=T))
 }
-ggplot.resphist <- function(result, groupVar = NULL, addStim = FALSE,
+ggplot.resphist <- function(result, groupVar = NULL, facetVar=NULL, addStim = FALSE,
     scaleDensity=TRUE, weights = c("all", "positive", "negative"),
     xlab = NULL, main = NULL, ylab = NULL, whichRes = NULL,
     dim = NULL, ...){
@@ -33,17 +33,17 @@ ggplot.resphist <- function(result, groupVar = NULL, addStim = FALSE,
     main <- ifelse(is.null(main), "", main)
     if(is.null(groupVar)& is.null(facetVar)){
         if(w == "all"){
-#        g <- ggplot(v, aes(x=idealpt)) +  xlab(xl) + ylab(yl) + ggtitle(main) +stat_density(geom="line") + theme_bw()
+        g <- ggplot(v, aes(x=idealpt)) +  xlab(xl) + ylab(yl) + ggtitle(main) +stat_density(geom="line") + theme_bw()
         }
         if(w == "positive"){
             posv <- v[which(v$weight > 0),]
             xl <- paste0(xl, " (n=", nrow(posv), ")")
-#            g <- ggplot(posv, aes(x=idealpt)) +  xlab(xl) + ylab(yl) + ggtitle(main) + stat_density(geom="line")  + theme_bw()
+            g <- ggplot(posv, aes(x=idealpt)) +  xlab(xl) + ylab(yl) + ggtitle(main) + stat_density(geom="line")  + theme_bw()
         }
         if(w == "negative"){
             negv <- v[which(v$weight < 0),]
             xl <- paste0(xl, " (n=", nrow(negv), ")")
-#            g <- ggplot(negv, aes(x=idealpt)) +  xlab(xl) + ylab(yl) + ggtitle(main)+ stat_density(geom="line")  + theme_bw()
+            g <- ggplot(negv, aes(x=idealpt)) +  xlab(xl) + ylab(yl) + ggtitle(main)+ stat_density(geom="line")  + theme_bw()
         }
     }
     else{
@@ -59,7 +59,7 @@ ggplot.resphist <- function(result, groupVar = NULL, addStim = FALSE,
             }
             for(i in 1:length(bd)){
                 if(scaleDensity)bd[[i]]$y <- bd[[i]]$y*props[i]
-                bd[[i]]$newg <- names(bd)[1]
+                bd[[i]]$newg <- names(bd)[i]
             }
             bd <- lapply(bd, function(z)data.frame("idealpt" = z$x, "Density"=z$y, "newg"=z$newg))
             bd <- do.call(rbind, bd)
@@ -67,8 +67,8 @@ ggplot.resphist <- function(result, groupVar = NULL, addStim = FALSE,
             t1 <- as.data.frame(t1)
             names(t1) <- c(gv)
             bd <- cbind(bd, t1)
-#            g <- ggplot(bd, aes(x=idealpt, y=Density, group=stimulus, color=stimulus)) + geom_line() + scale_color_manual(values=gray.palette(ng)) +
-#                 xlab(xl) + ylab(yl) + ggtitle(main) + theme_bw()
+           g <- ggplot(bd, aes(x=idealpt, y=Density, group=stimulus, color=stimulus)) + geom_line() + scale_color_manual(values=gray.palette(ng)) +
+                xlab(xl) + ylab(yl) + ggtitle(main) + theme_bw()
         }
         if(w == "positive"){
             posv <- v[which(v$weight > 0),]
@@ -86,8 +86,8 @@ ggplot.resphist <- function(result, groupVar = NULL, addStim = FALSE,
                 }
             bd <- lapply(bd, function(z)data.frame("idealpt" = z$x, "Density"=z$y, "stimulus"=z$stimulus))
             bd <- do.call(rbind, bd)
- #           g <- ggplot(bd, aes(x=idealpt, y=Density, group=stimulus, color=stimulus)) + geom_line() + scale_color_manual(values=gray.palette(ng)) +
- #               xlab(xl) + ylab(yl) + ggtitle(main) + theme_bw()
+           g <- ggplot(bd, aes(x=idealpt, y=Density, group=stimulus, color=stimulus)) + geom_line() + scale_color_manual(values=gray.palette(ng)) +
+               xlab(xl) + ylab(yl) + ggtitle(main) + theme_bw()
         }
         if(w == "negative"){
             negv <- v[which(v$weight < 0),]
@@ -105,8 +105,8 @@ ggplot.resphist <- function(result, groupVar = NULL, addStim = FALSE,
             }
             bd <- lapply(bd, function(z)data.frame("idealpt" = z$x, "Density"=z$y, "stimulus"=z$stimulus))
             bd <- do.call(rbind, bd)
-#            g <- ggplot(bd, aes(x=idealpt, y=Density, group=stimulus, color=stimulus)) + geom_line() + scale_color_manual(values=gray.palette(ng)) +
-#                 xlab(xl) + ylab(yl) + ggtitle(main) + theme_bw()
+           g <- ggplot(bd, aes(x=idealpt, y=Density, group=stimulus, color=stimulus)) + geom_line() + scale_color_manual(values=gray.palette(ng)) +
+                xlab(xl) + ylab(yl) + ggtitle(main) + theme_bw()
         }
     }
     if(addStim){
@@ -115,21 +115,21 @@ ggplot.resphist <- function(result, groupVar = NULL, addStim = FALSE,
         tmp <- tmp[which(names(tmp) %in% unique(groupVar))]
         n <- names(tmp)
         p <- data.frame("idealpt" = tmp, "stimulus" = factor(n, levels=n[order(tmp)]))
-#        g <- g + geom_point(data=p, aes(y=0, group=stimulus, pch=stimulus, col=stimulus, size=2.5)) +
-#            scale_shape_manual(values=shapes[1:nrow(p)]) + theme_bw() + scale_size(2.5, guide=FALSE)
+       g <- g + geom_point(data=p, aes(y=0, group=stimulus, pch=stimulus, col=stimulus, size=2.5)) +
+           scale_shape_manual(values=shapes[1:nrow(p)]) + theme_bw() + scale_size(2.5, guide=FALSE)
     }
     else{
         n <- names(tmp)
         p <- data.frame("idealpt" = tmp, "stimulus" = factor(n, levels=n[order(tmp)]))
-#        g <- g + geom_point(data=p, aes(y=0, group=stimulus, pch=stimulus, col=stimulus, size=2.5)) +
-#            scale_shape_manual(values=shapes[1:nrow(p)]) + scale_color_manual(values=gray.palette(nrow(p))) +
-#            theme_bw() + scale_size(2.5, guide=FALSE)
+       g <- g + geom_point(data=p, aes(y=0, group=stimulus, pch=stimulus, col=stimulus, size=2.5)) +
+           scale_shape_manual(values=shapes[1:nrow(p)]) + scale_color_manual(values=gray.palette(nrow(p))) +
+           theme_bw() + scale_size(2.5, guide=FALSE)
     }
     }
     if(!is.null(facetVar)){
-#        g <- g + facet_wrap(~facet)
+       g <- g + facet_wrap(~facet)
     }
-    # return(g)
+    return(g)
     if(exists("bd")){
         out <- list(data=bd)
     }
