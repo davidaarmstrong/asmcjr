4ac0ab32d687a266a3e26dd19c53c10cca66131a
David Armstrong <david@Davids-iMac-2.local>
2019-12-18 14:46:53 -0500

    Added the add.OCcutlines function into the code base because it's not exported from oc, so I can't import it here.

--- R/asmcjr.r
+++ R/asmcjr.r
@@ -1381,3 +1381,34 @@ plot.OCcutlines2 <- function (x, main.title = "Cutting Lines", d1.title = "First
   }
   suppressWarnings(apply(cutlineData, 1, oc:::add.OCcutline, lwd = lwd))
 }  
+
+add.OCcutline <- function(cutData,lwd=2) {
+
+    slope <- -cutData[1]/cutData[2]
+    if (is.na(slope)) {
+        x <- c(cutData[1]*cutData[3],cutData[1]*cutData[3])
+            y <- c(sqrt(1-(cutData[1]*cutData[3])^2),-sqrt(1-(cutData[1]*cutData[3])^2))
+                slope <- NA
+                intercept <- NA
+        }
+        else {
+                intercept <- -slope*cutData[1]*cutData[3]+cutData[2]*cutData[3]
+                x <- c( (-slope*intercept + sqrt( (slope*intercept)^2 -
+            (1+slope*slope)*(intercept*intercept-1)))/(1+slope*slope),
+                    (-slope*intercept - sqrt( (slope*intercept)^2 - 
+            (1+slope*slope)*(intercept*intercept-1)))/(1+slope*slope) )
+            if (is.na(x[1])) {
+                warning("Couldn't solve for points on the unit circle!\n")
+                x<-NA
+                y<-NA
+                slope<-NA
+                intercept<-NA  
+            }             
+            else {
+                y <- intercept + slope*x
+                y[y < -1] <- -sqrt(1-x[y<1]^2)
+                y[y >  1] <-  sqrt(1-x[y>1]^2)
+            }
+        }
+    lines(x,y,lwd=lwd)
+}
