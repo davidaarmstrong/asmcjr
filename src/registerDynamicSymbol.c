89fadd0bb7930eb6a90b5d7660a4c8b8c75d6589
David Liao <35374714+yl17124@users.noreply.github.com>
2020-05-11 19:30:45 +0100

    add `registerDynamicSymbol.c` in src
    I refered a solution from:" https://stackoverflow.com/questions/42313373/r-cmd-check-note-found-no-calls-to-r-registerroutines-r-usedynamicsymbols"  to fix an error of  "File ‘pkgname/libs/pkgname.so’:
      Found no calls to: ‘R_registerRoutines’, ‘R_useDynamicSymbols’".

---
+++ src/registerDynamicSymbol.c
@@ -0,0 +1,13 @@
+// RegisteringDynamic Symbols
+
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+void R_init_asmcjr(DllInfo* info) {
+	R_registerRoutines(info, NULL, NULL, NULL, NULL);
+	R_useDynamicSymbols(info, TRUE);
+}
+
+// RegisteringDynamic Symbols
+
