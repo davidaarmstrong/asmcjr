53377e585fe0badcd75e7b7a43c2b83cf28fbede
David Liao <35374714+yl17124@users.noreply.github.com>
2020-05-12 13:44:33 +0100

    update

--- R/header.R
+++ R/header.R
@@ -1,14 +1,10 @@
-.onAttach <- function(...) {
+.onLoad <- function(...) {
 
-   mydate <- date()
-   x <- regexpr("[0-9]{4}", mydate)
-   this.year <- substr(mydate, x[1], x[1] + attr(x, "match.length") - 1)
-
-   packageStartupMessage("\n## This package supports the book Analyzing Spatial Models of Choice and Judgment with R") 
-   #packageStartupMessage("## Copyright 2013 - ", this.year)
-   packageStartupMessage("## Authors: David A. Armstrong, II, Ryan Bakker, Royce Carroll, Christopher Hare, Keith T. Poole, Howard Rosenthal (2020)")
-   packageStartupMessage("## Pacakge creator: David A. Armstrong")
-   packageStartupMessage("## Pacakge contributors: David A. Armstrong (https://github.com/davidaarmstrong/asmcjr), Yen-Chieh Liao (https://github.com/yl17124/asmcjr)")
+   packageStartupMessage("\n ## This package supports the book Analyzing Spatial Models of Choice and Judgment with R") 
+   packageStartupMessage("\n ## Authors: David A. Armstrong, II, Ryan Bakker, Royce Carroll, Christopher Hare, Keith T. Poole, Howard Rosenthal (2020)")
+   packageStartupMessage("\n ## Pacakge creator: David A. Armstrong")
+   packageStartupMessage("\n ## Pacakge contributors: David A. Armstrong (https://github.com/davidaarmstrong/asmcjr), Yen-Chieh Liao (https://github.com/yl17124/asmcjr)")
+   packageStartupMessage("\n ## Installation succeeded via https://github.com/yl17124/asmcjr on ", date())
 
 }
 
